be8237522eb650c25898d9dff9f36bfd985a9799
AJ9891 <161670894+AJ9891@users.noreply.github.com>
2026-08-12 10:38:05 -0700

    preserve lockfile-compatible Stripe API version

--- apps/web/src/lib/stripe.ts
+++ apps/web/src/lib/stripe.ts
@@ -7,7 +7,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 
 export const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2026-02-25.clover',
+      apiVersion: '2025-12-15.clover',
     })
   : null
 
